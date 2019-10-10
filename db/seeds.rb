3e0295c425d9b2a85ab41de31657ade7462337cd
Achmad Rifaldi <achmadrifaldi@makmal.xyz>
2019-10-10 16:35:48 +0700

    Add Seeder for AppOption & Product

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.connection.execute("TRUNCATE app_options RESTART IDENTITY CASCADE")
+AppOption.create!([
+  {
+    option_name: 'name',
+    option_value: 'Belanja API Demo'
+  },
+  {
+    option_name: 'version',
+    option_value: '1.0.0'
+  }
+])
+
+ActiveRecord::Base.connection.execute("TRUNCATE products RESTART IDENTITY CASCADE")
+Product.create!([
+  {
+    name: 'Sandal Mahal',
+    price: 5000000,
+    image: 'https://anu.com/images/sandal-mahal.jpg',
+  },
+  {
+    name: 'Baju Mahal',
+    price: 8000000,
+    image: 'https://anu.com/images/baju-mahal.jpg',
+  }
+])
