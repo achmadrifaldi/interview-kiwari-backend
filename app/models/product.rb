a8fb9d82cf23b18d910f6422a34129d6a7b6dc6e
Achmad Rifaldi <achmadrifaldi@makmal.xyz>
2019-10-10 17:17:06 +0700

    Add validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,18 @@
 #  updated_at :datetime         not null
 #
 
-class Product < ApplicationRecord; end
+class Product < ApplicationRecord
+	validates :name, 
+		presence: true,
+		length: { maximum: 255 }
+	
+	validates :price, 
+		presence: true,
+		numericality: {
+			only_integer: true,
+			greater_than: 0
+		}
+
+	validates :image, 
+		url: { allow_blank: true }
+end
